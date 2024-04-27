ee1f06bf528a01c3d8dca31f443d9bd0923cf590
Anmol Garg <anmolgarg271005@gmail.com>
2024-04-27 10:33:50 +0530

    "Added env"

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose")
 const userRoute = require("./routes/userRoute")
 const accountRoute = require("./routes/accountRoute")
 const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config();
 const app = express();
 
 app.use(cors());
@@ -10,9 +12,9 @@ app.use(express.json());
 app.use("/api/v1/user" , userRoute)
 app.use("/api/v1/account" , accountRoute)
 
-mongoose.connect("mongodb://127.0.0.1:27017/paytm")
+mongoose.connect(process.env.MONGO_URI)
     .then(()=>{console.log("Mongodb connected")})
 
-app.listen(4000 , ()=>{
+app.listen(process.env.PORT , ()=>{
     console.log("Server started")
 })
